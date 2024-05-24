b057630ca3598955b1694a584de0f36a35c49b3c
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-05-23 22:22:58 -0300

    Inserimos os botoes da calculadora, realizamos os ajustes de tamanho e identificação de cores dos botões da direita.

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 import Botao from "./src/components/Botao";
 
 export default props => {
-    return (
-                <SafeAreaView style={estilos.container}>
-          <Botao/>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.botao}>
+        <Botao label="AC" triplo/>
+        <Botao label="/" dir/>
+        <Botao label="7" />
+        <Botao label="8" />
+        <Botao label="9" />
+        <Botao label="*" dir/>
+        <Botao label="4" />
+        <Botao label="5" />
+        <Botao label="6" />
+        <Botao label="-" dir/>
+        <Botao label="1" />
+        <Botao label="2" />
+        <Botao label="3" />
+        <Botao label="+" dir/>
+        <Botao label="0" duplo/>
+        <Botao label="." />
+        <Botao label="=" dir/>
+        
+
+      </View>
+    </SafeAreaView>
+  )
 }
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#DB7093",
-    justifyContent: "center",
-    alignItems:"center"
+    backgroundColor: "#FF69B4",
+    justifyContent: "flex-end"
 
+  },
+  botao: {
+    flexDirection: "row",
+    flexWrap: "wrap"
   }
+
+
 })
--- src/components/Botao.js
+++ src/components/Botao.js
@@ -1,10 +1,19 @@
 import{Text, 
-    TouchableHighlight, StyleSheet} from "react-native"
+    TouchableHighlight, StyleSheet, Dimensions} from "react-native"
+
+    const{width, height} = Dimensions.get("window")
+
 
 export default props => {
+
+    const estilosBotao = [estilos.botao]
+    if(props.duplo) estilosBotao.push(estilos.botaoDuplo)
+    if(props.triplo) estilosBotao.push(estilos.botaoTriplo) /* chama o botao e adiciona o estilo */
+    if(props.dir) estilosBotao.push(estilos.botaoDir) /* chama os botoes da direita e deixa com a cor diferente */
+
     return(
         <TouchableHighlight>
-            <Text style={estilos.botao}>9</Text>
+            <Text style={estilosBotao}> {props.label}</Text>
         </TouchableHighlight>
         
     )
@@ -13,13 +22,22 @@ export default props => {
 const estilos = StyleSheet.create({
     botao: {
         fontSize:40,
-        width: 100,
-        height:100,
+        width: width /4,
+        height: width/4,
         padding: 20,
         backgroundColor: "#f0f0f0",
         textAlign: "center",
         borderWidth: 1,
-        borderColor: "#E6E6FA"
+        borderColor: "#C71585"
 
+    },
+     botaoDuplo: {
+        width: width /4 *2    
+      }, 
+      botaoTriplo: {
+        width: width /4 *3
+      },
+    botaoDir: {
+        backgroundColor: "#DA70D6",
     }
 })
