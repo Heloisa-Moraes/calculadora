4c7b83a2a8a0bb8f1cc491eed21b31bd052e08e4
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-06-04 22:25:38 -0300

    adicionado novas funções aos botoes 04-06

--- App.js
+++ App.js
@@ -2,11 +2,40 @@ import { SafeAreaView, StyleSheet, View } from "react-native";
 
 import Botao from "./src/components/Botao";
 import Visor from "./src/components/Visor";
+import { useState } from "react";
+
+const estadoInicial = {
+  valorVisor: "0",
+  limparVisor: false,
+  operacao: null,
+  valores: [0, 0],
+  posicaoCorrente: 0
+}
+
+let estadoTemp = {
+  valorVisor: "0",
+  limparVisor: false,
+  operacao: null,
+  valores: [0, 0],
+  posicaoCorrente: 0
+}
 
 export default props => {
 
+  const [estadoCalculadora, setEstadoCalculadora] = useState({...
+  estadoInicial})
+
   adicionarDigito = n => {
-    console.warn(n)
+    if(n === '.' && estadoCalculadora.valorVisor.includes('.')) {
+      return;
+    }
+
+    const limparVisor = estadoCalculadora.valorVisor === '0' || 
+    estadoCalculadora.limparVisor
+    const valorCorrente = limparVisor ? '' : estadoCalculadora.valorVisor
+    const valorVisor = valorCorrente + n 
+    estadoTemp = {...estadoCalculadora,valorVisor,limparVisor: false}
+    setEstadoCalculadora({...estadoTemp})
   }
 
   limparMemoria = () => {
@@ -18,25 +47,25 @@ export default props => {
   }
   return (
     <SafeAreaView style={estilos.container}>
-      <Visor />
+      <Visor valor={estadoCalculadora.valorVisor}/>
       <View style={estilos.botao}>
         <Botao label="AC" onClick={limparMemoria} triplo/>
-        <Botao label="/" onClick={setOperacao}/>
+        <Botao label="/" onClick={setOperacao} dir/>
         <Botao label="7" onClick={adicionarDigito}/>
         <Botao label="8"onClick={adicionarDigito} />
         <Botao label="9" onClick={adicionarDigito}/>
-        <Botao label="*" onClick={setOperacao}/>
+        <Botao label="*" onClick={setOperacao} dir/>
         <Botao label="4" onClick={adicionarDigito}/>
         <Botao label="5" onClick={adicionarDigito}/>
         <Botao label="6" onClick={adicionarDigito}/>
-        <Botao label="-" onClick={setOperacao}/>
+        <Botao label="-" onClick={setOperacao} dir/>
         <Botao label="1" onClick={adicionarDigito}/> 
         <Botao label="2" onClick={adicionarDigito}/>
         <Botao label="3" onClick={adicionarDigito}/>
-        <Botao label="+" onClick={setOperacao}/>
+        <Botao label="+" onClick={setOperacao}dir/>
         <Botao label="0" onClick={adicionarDigito} duplo/>
         <Botao label="." onClick={adicionarDigito}/>
-        <Botao label="=" onClick={setOperacao}/>
+        <Botao label="=" onClick={setOperacao}dir/>
         
 
       </View>
@@ -46,7 +75,7 @@ export default props => {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FF69B4",
+    backgroundColor: "#FF1493",
     justifyContent: "flex-end"
 
   },
